827ed8967f96fa65fd62db79d3762ad9d46e51f0
Nicolas Tollenaere <nicolas.tollenaere@inria.fr>
2016-07-21 16:12:30 +0200

    compare script

--- diff-threads/compare-results.py
+++ diff-threads/compare-results.py
@@ -7,4 +7,25 @@ Mlimit = 16.0
 Climit = 25.0
 
 
-subprocess.run(["./execute-load"])
+output = subprocess.run(["sh", "./exec-load.sh"], stdout=subprocess.PIPE, universal_newlines=True)
+
+nm = 0
+nc = 0
+
+lines = output.stdout.splitlines()
+
+for line in lines:
+    if (float(line) > Climit):
+        nc += 1
+    elif (float(line) < Mlimit):
+        nm += 1
+
+
+print("{0} threads in full hits".format(nc))
+print("{0} threads in full miss".format(nm))
+
+output = subprocess.run(["sh", "./eval-cache.sh"], stdout=subprocess.PIPE, universal_newlines=True)
+
+
+expect_hits = int(output.stdout)
+print("expected {0} full hits threads".format(expect_hits))
