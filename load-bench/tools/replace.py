19d2ea4b388dc487e34cc6d2cf9f1102b006ffe6
Nicolas Tollenaere <nicolas.tollenaere@inria.fr>
2016-09-05 17:04:54 +0200

    mini-start of replacement algo

---
+++ load-bench/tools/replace.py
@@ -0,0 +1,7 @@
+import math
+
+class Cacheset:
+
+    def __init__(self, nbLine):
+        self.lines = [0] * nbLine
+
---
+++ load-bench/tools/replace_algo.ml
@@ -0,0 +1,4 @@
+type BinTree = Node of bool * BinTree * BinTree | Leaf of bool
+
+
+type CacheSet = {lines : int[]}
